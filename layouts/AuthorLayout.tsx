1f86c0c2fc59e7be154a6560213be49c745dbb97
Yevhen Yeshchev <yyeshchev@gmail.com>
2023-02-19 20:09:06 +0200

    AuthorLayour: made avatar bigger

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -18,7 +18,7 @@ export default function AuthorLayout({ children, content }: Props) {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+            About me
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
@@ -26,9 +26,9 @@ export default function AuthorLayout({ children, content }: Props) {
             <Image
               src={avatar}
               alt="avatar"
-              width={192}
-              height={192}
-              className="h-48 w-48 rounded-full"
+              width={240}
+              height={240}
+              className="h-60 w-60 rounded-full"
             />
             <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
