52097d2f039d6bf4da6e4afb1b470f18e523491d
Yevhen Yeshchev <yyeshchev@gmail.com>
2023-02-21 17:28:28 +0200

    added robots.txt, updated footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,11 +14,11 @@ export default function Footer() {
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
         </div>
-        <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+        <div className="mb-2 flex space-x-1 sm:space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
-          <div>{` • `}</div>
+          <div>{` | `}</div>
           <div>{`Copyright © ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
+          <div>{` | `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
         <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
