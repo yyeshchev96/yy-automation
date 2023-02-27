5414e439362fbdf7af6a6e32c5310a29c15f3907
Yevhen Yeshchev <yyeshchev@gmail.com>
2023-02-27 21:33:33 +0200

    updated post layout

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function PageTitle({ children, extra }: Props) {
   return (
-    <h1 className="text-5xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl sm:leading-10 md:text-6xl md:leading-14">
+    <h1 className="text-5xl font-extrabold leading-12 tracking-tight text-gray-900 dark:text-gray-100 sm:text-5xl sm:leading-12 md:text-6xl md:leading-14">
       <GradientText>{children}</GradientText>
       {extra ? extra : ''}
     </h1>
