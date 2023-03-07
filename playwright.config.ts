d1d9b78e40f81fae7a2ebc4453a52423d30a4cd2
Yevhen Yeshchev <38920620+yyeshchev96@users.noreply.github.com>
2023-03-07 12:34:18 +0200

    added playwright page for home (#6)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test'
 
 /**
  * Read environment variables from file.
@@ -18,7 +18,7 @@ export default defineConfig({
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 5000,
   },
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -33,7 +33,6 @@ export default defineConfig({
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-  
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.CI ? process.env.BASE_URL : 'http://localhost:3000',
 
@@ -90,5 +89,6 @@ export default defineConfig({
     command: 'npm run start',
     port: 3000,
     reuseExistingServer: !process.env.CI,
+    timeout: 10 * 1000,
   },
-});
+})
